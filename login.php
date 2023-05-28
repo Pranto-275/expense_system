a4aeb20955d3320148c465fdef507babe1b054a9
Pranto-275 <pranto.rahaman38@gmail.com>
2023-05-28 16:15:53 +0600

    login and register page convert into php

---
+++ login.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div id="loginhome">
+        <div class="login">
+            <h3 class="py-4">Daily Expense Tracker</h1>
+                <div class="box">
+                    <form>
+                        <div class="mb-3">
+                          <label for="exampleInputEmail1" class="form-label">Email address</label>
+                          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                         
+                        </div>
+                        <div class="mb-3">
+                          <label for="exampleInputPassword1" class="form-label">Password</label>
+                          <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <button type="submit" class="btn w-100" style="background-color: #4942E4;"><b>Submit</b></button>
+                        <div style="text-align: center;color: black;">
+                            <a style="color:black;" href="register.html">Create your Account...</a>
+                        </div>
+                      </form>
+                </div>
+            
+        </div>
+    </div>
+
+    
+   <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register Users</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div id="loginhome">
+        <div class="login">
+            <h3 class="py-4">Daily Expense Tracker</h1>
+                <div class="box">
+                    <form>
+                      <div class="mb-3">
+                        <label  class="form-label">Full name</label>
+                        <input type="text" class="form-control" id="fullname">
+                       
+                      </div>
+                      <div class="mb-3">
+                        <label class="form-label">Email address</label>
+                        <input type="email" class="form-control" >
+                       
+                      </div>
+                        <div class="mb-3">
+                          <label  class="form-label">Phone</label>
+                          <input type="number" class="form-control" id="phone">
+                         
+                        </div>
+                        <div class="mb-3">
+                          <label class="form-label">Password</label>
+                          <input type="password" class="form-control">
+                        </div>
+
+                        <div class="mb-3">
+                          <label class="form-label">Confirm Password</label>
+                          <input type="password" class="form-control">
+                        </div>
+                  
+                        <button type="submit" class="btn w-100" style="background-color: #4942E4;"><b>Submit</b></button>
+
+                        <div style="text-align: center;color: black;">
+                          <a style="color:black;" href="login.html">Have an Account...</a>
+                      </div>
+                      </form>
+                </div>
+            
+        </div>
+    </div>
+
+    
+   <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
